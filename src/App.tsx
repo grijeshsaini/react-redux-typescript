15b7360138aaf2312a19ce310c5cc6cdd01c410f
grijeshsaini <grijesh.rajoria@gmail.com>
2022-09-24 00:31:02 +0100

    Prep Work

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,18 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <h1>Welcome !!!!</h1>
+            <div>
+                <div className={"row"}>
+                    <span className={"value"}>Balance: {10}</span>
+                </div>
+                <div className={"row"}>
+                    <input className={"textbox"} value={0}/>
+                </div>
+                <div className={"row"}>
+                    <button className={"button"}>Deposit Amount</button>
+                    <button className={"button"}>Withdraw Amount</button>
+                </div>
+            </div>
         </div>
     );
 }
