bbfee6fe64ea65bfd4277c7bcb3f8a241b28eb03
grijeshsaini <grijesh.rajoria@gmail.com>
2022-09-24 01:09:43 +0100

    React-Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+    const [balance, setBalance] = useState(0);
+    const [amount, setAmount] = useState('0');
     return (
         <div className="App">
             <div>
                 <div className={"row"}>
-                    <span className={"value"}>Balance: {10}</span>
+                    <span className={"value"}>Balance: {balance}</span>
                 </div>
                 <div className={"row"}>
-                    <input className={"textbox"} value={0}/>
+                    <input className={"textbox"} value={amount} onChange={(e) => setAmount(e.target.value)}/>
                 </div>
                 <div className={"row"}>
-                    <button className={"button"}>Deposit Amount</button>
-                    <button className={"button"}>Withdraw Amount</button>
+                    <button className={"button"}
+                            onClick={() => setBalance(balance + (Number(amount) || 0))}>
+                        Deposit Amount
+                    </button>
+                    <button className={"button"}
+                            onClick={() => setBalance(balance - (Number(amount) || 0))}>
+                        Withdraw Amount
+                    </button>
                 </div>
             </div>
         </div>
